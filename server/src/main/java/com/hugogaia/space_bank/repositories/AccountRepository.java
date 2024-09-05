c3d2e52a15f0f62bd578cef1e0f954208fc6aa93
Hugo Gaia <hugogaia65@gmail.com>
2024-09-05 03:35:56 -0300

    feat(findById): find account by id

--- server/src/main/java/com/hugogaia/space_bank/repositories/AccountRepository.java
+++ server/src/main/java/com/hugogaia/space_bank/repositories/AccountRepository.java
@@ -3,7 +3,12 @@ package com.hugogaia.space_bank.repositories;
 import com.hugogaia.space_bank.models.AccountModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AccountRepository extends JpaRepository<AccountModel, Long> {
+
+
+    Optional<AccountModel> findById(Long id);
     AccountModel findByEmail(String email);
     AccountModel findByTaxId(String taxId);
 
