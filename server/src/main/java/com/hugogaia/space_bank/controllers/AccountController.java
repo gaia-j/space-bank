be9bd22d1488bf3a5613d03b635abb1aad662dfb
Hugo Gaia <hugogaia65@gmail.com>
2024-09-06 14:08:44 -0300

    feat(detailAccount): detail account by account code

--- server/src/main/java/com/hugogaia/space_bank/controllers/AccountController.java
+++ server/src/main/java/com/hugogaia/space_bank/controllers/AccountController.java
@@ -1,21 +1,26 @@
 package com.hugogaia.space_bank.controllers;
 
 import com.hugogaia.space_bank.models.AccountModel;
+import com.hugogaia.space_bank.repositories.AccountRepository;
 import com.hugogaia.space_bank.services.AuthorizationService;
 import com.hugogaia.space_bank.utils.TaxIdUtils;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
+import java.util.UUID;
 
 @RestController
 public class AccountController {
 
     private final AuthorizationService authorizationService;
+    private final AccountRepository accountRepository;
 
-    public AccountController(AuthorizationService authorizationService) {
+    public AccountController(AuthorizationService authorizationService, AccountRepository accountRepository) {
         this.authorizationService = authorizationService;
+        this.accountRepository = accountRepository;
     }
 
     @GetMapping("/account")
@@ -37,19 +42,18 @@ public class AccountController {
     }
 
     @GetMapping("/account/{accountCode}")
-    public ResponseEntity<Map<String,Object>> detailAccountByCode(HttpServletRequest request) {
+    public ResponseEntity<Map<String,Object>> detailAccountByCode(@PathVariable String accountCode, HttpServletRequest request) {
 
         AccountModel account = authorizationService.authorize(request);
 
-        String taxId = TaxIdUtils.hideTaxId(account.getTaxId());
+        AccountModel desiredAccount = accountRepository.findByAccountCode(accountCode);
+
+        String taxId = TaxIdUtils.hideTaxId(desiredAccount.getTaxId());
 
         return ResponseEntity.ok(Map.of(
-                "accountCode", account.getAccountCode(),
-                "name", account.getName(),
-                "email", account.getEmail(),
-                "taxId", taxId,
-                "balance", account.getBalance(),
-                "birthDate", account.getBirthDate()
+                "accountCode", desiredAccount.getAccountCode(),
+                "name", desiredAccount.getName(),
+                "taxId", taxId
         ));
 
     }
